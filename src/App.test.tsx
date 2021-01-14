c1fee8fc1e93661074e982ae381a6df65fa4814c
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-14 18:22:46 +0000

    added global styles and theme provider

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,6 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-
 test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
--- src/App.tsx
+++ src/App.tsx
@@ -2,56 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-
+import {ThemeProvider} from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyles from "./styles/GlobalStyle";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      <ThemeProvider theme={theme}>
+          <GlobalStyles/>
+      <h1>Har</h1>
+      </ThemeProvider>
   );
 }
 
