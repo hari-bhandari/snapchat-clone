443547f04f08a0662ddaa01f3c1ca1e1745a914f
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-16 19:16:00 +0000

    integrated webcam

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,28 @@ import {ThemeProvider} from "styled-components";
 import theme from "./styles/theme";
 import GlobalStyles from "./styles/GlobalStyle";
 import WebcamCapture from "./components/webcam/WebcamCapture";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+import {AppBody} from "./AppCSS";
+
 function App() {
-  return (
-      <ThemeProvider theme={theme}>
-          <GlobalStyles/>
-        <WebcamCapture/>
-      </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyles/>
+            <Router>
+                <AppBody>
+            <Switch>
+                <Route path="/">
+                    <WebcamCapture/>
+                </Route>
+            </Switch>
+                </AppBody>
+    </Router>
+</ThemeProvider>
+);
 }
 
 export default App;
