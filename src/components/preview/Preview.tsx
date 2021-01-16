e46f18d77967d8ba8703ba85c60f342a2859237c
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-16 20:28:39 +0000

    pushed to homepage if image is non existant

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {PreviewContainer} from "./PreviewCSS";
 import {useSelector} from "react-redux";
 import {selectCameraImage} from "../../features/camera/cameraSlice";
+import {useHistory} from "react-router-dom";
 
 const Preview:React.FC = () => {
     const cameraImage=useSelector(selectCameraImage)
+    const history=useHistory()
+    useEffect(()=>{
+        if(cameraImage===''){
+            history.push('/')
+        }
+    },[])
     return (
         <PreviewContainer>
             <img src={cameraImage} alt=""/>
