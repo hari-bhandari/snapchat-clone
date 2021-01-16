4a0e52984de718537fe481f0f183d600e933697b
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-16 20:41:07 +0000

    styled preview page

--- src/components/preview/PreviewCSS.ts
+++ src/components/preview/PreviewCSS.ts
@@ -1,4 +1,12 @@
 import styled from "styled-components";
 export const PreviewContainer=styled.div`
-    
+  position: relative;
+  .close{
+    position: absolute;
+    top: 0;
+    margin: 5px;
+    cursor: pointer;
+    color: white;
+  }
+  
 `
