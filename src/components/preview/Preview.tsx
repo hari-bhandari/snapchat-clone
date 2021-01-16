94ef5e1b38b1d26abde4ddc1f957e5debd4d61ca
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-16 20:36:09 +0000

    added close preview and used redirection

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -1,19 +1,25 @@
 import React, {useEffect} from 'react';
 import {PreviewContainer} from "./PreviewCSS";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {selectCameraImage} from "../../features/camera/cameraSlice";
 import {useHistory} from "react-router-dom";
-
+import CloseIcon from '@material-ui/icons/Close'
+import {resetCameraImage} from '../../features/camera/cameraSlice'
 const Preview:React.FC = () => {
+    const dispatch=useDispatch()
     const cameraImage=useSelector(selectCameraImage)
     const history=useHistory()
     useEffect(()=>{
         if(cameraImage===''){
-            history.push('/')
+            history.replace('/')
         }
-    },[])
+    },[cameraImage,history])
+    const closePreview=()=>{
+        dispatch(resetCameraImage())
+    }
     return (
         <PreviewContainer>
+            <CloseIcon className={'close'} onClick={closePreview}/>
             <img src={cameraImage} alt=""/>
         </PreviewContainer>
     );
