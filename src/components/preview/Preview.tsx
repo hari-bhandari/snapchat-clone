d1101a68c70353f79f4af0b5fe7338a322ddb53f
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-18 17:12:37 +0000

    added send photo function to save photo and store it on database

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -1,9 +1,12 @@
 import React, {useEffect} from 'react';
-import {PreviewContainer,ToolbarRight,Footer} from "./PreviewCSS";
+import {PreviewContainer, ToolbarRight, Footer} from "./PreviewCSS";
 import {useDispatch, useSelector} from "react-redux";
 import {selectCameraImage} from "../../features/camera/cameraSlice";
 import {useHistory} from "react-router-dom";
 import {resetCameraImage} from '../../features/camera/cameraSlice'
+import {v4 as uuid} from "uuid"
+import {db, storage} from "../../firebase";
+import firebase from "firebase";
 //material ui icons
 import CloseIcon from '@material-ui/icons/Close'
 import TextFieldsIcon from '@material-ui/icons/TextFields';
@@ -14,17 +17,35 @@ import AttachFileIcon from '@material-ui/icons/AttachFile';
 import CropIcon from '@material-ui/icons/Crop';
 import TimerIcon from '@material-ui/icons/Timer';
 import SendIcon from '@material-ui/icons/Send'
-const Preview:React.FC = () => {
-    const dispatch=useDispatch()
-    const cameraImage=useSelector(selectCameraImage)
-    const history=useHistory()
-    useEffect(()=>{
-        if(cameraImage===''){
+
+const Preview: React.FC = () => {
+    const dispatch = useDispatch()
+    const cameraImage = useSelector(selectCameraImage)
+    const history = useHistory()
+    useEffect(() => {
+        if (cameraImage === '') {
             history.replace('/')
         }
-    },[cameraImage,history])
-    const closePreview=()=>{
+    }, [cameraImage, history])
+    const closePreview = () => {
         dispatch(resetCameraImage())
+    }
+    const sendPhoto = () => {
+        const id = uuid();
+        const uploadTask = storage.ref(`posts/${id}`).putString(cameraImage, 'data_url')
+        uploadTask.on('state_changed', null, (error => {
+            console.log(error)
+        }), () => {
+            storage.ref('posts').child(id).getDownloadURL().then((url)=>{
+                db.collection('posts').add({
+                    imageUrl:url,
+                    username:'HariBhandari',
+                    read:false,
+                    timestamp:firebase.firestore.FieldValue.serverTimestamp()
+                })
+                history.replace('/chats')
+            })
+        })
     }
     return (
         <PreviewContainer>
@@ -39,7 +60,7 @@ const Preview:React.FC = () => {
                 <TimerIcon/>
             </ToolbarRight>
             <img src={cameraImage} alt=""/>
-            <Footer>
+            <Footer onClick={sendPhoto}>
                 <h2>Send Now</h2>
                 <SendIcon fontSize={"small"} className="send"/>
             </Footer>
