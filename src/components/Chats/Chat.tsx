97fc46a78c876cd4831296e657d21e38b3ddfb3b
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-19 10:16:37 +0000

    added login logout , selected image and removed selected image on reducer

--- src/components/Chats/Chat.tsx
+++ src/components/Chats/Chat.tsx
@@ -3,13 +3,20 @@ import {ChatContainer,ChatInfo} from "./ChatsCss";
 import {Avatar} from "@material-ui/core";
 import {StopRounded} from "@material-ui/icons";
 import ReactTimeago from 'react-timeago'
+import {useDispatch} from "react-redux";
+
 interface DataProps{
     id:String,
     data:any
 }
 const Chat:React.FC<DataProps> = ({data:{id,data:{profilePic,username,timestamp,imageUrl,read}}}) => {
+    const open=()=>{
+        // if(!read){
+        //     dispatch(selectImage)
+        // }
+    }
     return (
-        <ChatContainer>
+        <ChatContainer onClick={open}>
             <Avatar className={"avatar"} src={profilePic}/>
             <ChatInfo>
                 <h4>{username}</h4>
--- src/features/counter/appSlice.ts
+++ src/features/counter/appSlice.ts
@@ -2,30 +2,42 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
 
 interface CounterState {
-  value: number;
+  user: object|null,
+  selectedImage:string|null
 }
 
 const initialState: CounterState = {
-  value: 0,
+  user: null,
+  selectedImage:null
 };
 
 export const appSlice = createSlice({
   name: 'app',
   initialState,
   reducers: {
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload;
+    login: (state, action: PayloadAction<object>) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+    selectImage: (state, action: PayloadAction<string>) => {
+      state.selectedImage = action.payload;
+    },
+    resetImage: (state) => {
+      state.selectedImage = null;
     },
   },
 });
 
-export const { incrementByAmount } = appSlice.actions;
+export const { login,logout,selectImage,resetImage } = appSlice.actions;
 
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 // @ts-ignore
-export const selectApp = (state: RootState) => state.app.value;
+export const selectUser = (state: RootState) => state.app.user;
+export const selectSelectedImage = (state: RootState) => state.app.selectedImage;
 
 export default appSlice.reducer;
