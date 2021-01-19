4d3a2c4ed7eacb8b4d4a5dd956996a830469b017
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-19 10:29:34 +0000

    added OnOpen method to open snaps

--- src/components/Chats/Chat.tsx
+++ src/components/Chats/Chat.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import {ChatContainer,ChatInfo} from "./ChatsCss";
+import {ChatContainer, ChatInfo} from "./ChatsCss";
 import {Avatar} from "@material-ui/core";
 import {StopRounded} from "@material-ui/icons";
 import ReactTimeago from 'react-timeago'
 import {useDispatch} from "react-redux";
+import {selectImage} from "../../features/counter/appSlice";
+import {db} from "../../firebase";
+import {useHistory} from "react-router-dom";
 
-interface DataProps{
-    id:String,
-    data:any
+interface DataProps {
+    id: String,
+    data: any
 }
-const Chat:React.FC<DataProps> = ({data:{id,data:{profilePic,username,timestamp,imageUrl,read}}}) => {
-    const open=()=>{
-        // if(!read){
-        //     dispatch(selectImage)
-        // }
+
+const Chat: React.FC<DataProps> = ({data: {id, data: {profilePic, username, timestamp, imageUrl, read}}}) => {
+    const dispatch = useDispatch()
+    const history=useHistory()
+    const open = () => {
+        if (!read) {
+            dispatch(selectImage(imageUrl))
+            db.collection('posts').doc(id).set({read: true}, {merge: true })
+            history.push('/chats/view')
+        }
     }
     return (
         <ChatContainer onClick={open}>
@@ -22,7 +30,7 @@ const Chat:React.FC<DataProps> = ({data:{id,data:{profilePic,username,timestamp,
                 <h4>{username}</h4>
                 <p>tap to view - <ReactTimeago date={new Date(timestamp?.toDate()).toUTCString()}/></p>
             </ChatInfo>
-            {!read&&<StopRounded className={"read"}/>}
+            {!read && <StopRounded className={"read"}/>}
         </ChatContainer>
     );
 };
